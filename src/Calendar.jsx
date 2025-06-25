8acb12e6e617d623c9c73e1747b65b394d7ab76e
Avanthikha B S <151737087+avi11204@users.noreply.github.com>
2025-06-25 16:20:37 +0530

    Update Calendar.jsx

--- src/Calendar.jsx
+++ src/Calendar.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import dayjs from "dayjs";
-import "./Calendar.css";
+import "./Calender.css";
 
 const Calendar = () => {
   const [currentDate, setCurrentDate] = useState(dayjs());
